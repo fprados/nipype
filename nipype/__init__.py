18c209c37c593c5d06f59dcaecc85d1efea32c57
Nicolas Toussaint <n.toussaint@ucl.ac.uk>
2014-05-01 12:07:29 +0100

    niftyseg-maths and niftyseg-stats are now fully interfaced in nipype

--- nipype/__init__.py
+++ nipype/__init__.py
@@ -54,8 +54,7 @@ def _test_local_install():
     if os.getcwd() == os.sep.join(
                             os.path.abspath(__file__).split(os.sep)[:-2]):
         import warnings
-        warnings.warn('Running the tests from the install directory may '
-                     'trigger some failures')
+        warnings.warn('Running the tests from the install directory may trigger some failures')
 
 _test_local_install()
 
